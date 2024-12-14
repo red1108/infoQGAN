9a3d169b3af721ea07067dcddbca4117a9029e06
Mingyu Lee <minkyu.lee65@gmail.com>
2024-12-14 23:55:45 +0900

    fix minor otypo in mine train

--- new_mnist_train.py
+++ new_mnist_train.py
@@ -430,9 +430,9 @@ for epoch in range(1, epoch_num+1):
         with torch.no_grad():
             reconstructed_images = autoencoder.decoder(generator_output)
         flattened_images = reconstructed_images.view(reconstructed_images.size(0), -1) # (BATCH_SIZE, 784)
-        pred_xy = mine(code_input, reconstructed_images)
+        pred_xy = mine(code_input, flattened_images)
         code_input_shuffle = code_input[torch.randperm(BATCH_SIZE)]
-        pred_x_y = mine(code_input_shuffle, reconstructed_images)
+        pred_x_y = mine(code_input_shuffle, flattened_images)
         mi = -torch.mean(pred_xy) + torch.log(torch.mean(torch.exp(pred_x_y)))
         M_opt.zero_grad()
         mi.requires_grad_(True)
