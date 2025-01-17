bd7b5f2e222c67beab485215c53fb5676f00c51b
red1108 <minkyu.lee65@gmail.com>
2025-01-17 11:58:27 +0900

    change to ry rz

--- modules/QGAN.py
+++ modules/QGAN.py
@@ -189,3 +189,49 @@ class QGAN4:
     
     def parameters(self):
         return [self.params]
+    
+
+class QGAN5:
+    def __init__(self, n_qubits, output_qubits, n_layers, params, dev):
+        self.n_qubits = n_qubits
+        self.n_layers = n_layers
+        self.params = params
+
+        self.output_qubits = output_qubits
+
+        if output_qubits > n_qubits:
+            raise ValueError("output_qubits should be smaller than n_qubits")
+        
+        self.dev = dev  # pennylane device
+        self.generator_circuit_qnode = qml.QNode(self.circuit, self.dev, interface="torch")
+        
+    def init_circuit(self, generator_input):
+        for i in range(self.n_qubits):
+            qml.RY(generator_input[i]*np.pi/2, wires=i) # TODO: *a 해서 값 범위 맞추기
+        
+    def single_layer(self, params, last=False):
+        for i in range(self.n_qubits):
+            qml.RY(params[i][0], wires=i)
+            qml.RZ(params[i][1], wires=i)
+        
+        if not last:
+            for i in range(self.n_qubits):
+                qml.CNOT(wires=[i, (i+1)%self.n_qubits])
+
+    def circuit(self, generator_input):
+        # output dimension: 2**output_qubits
+
+        self.init_circuit(generator_input)
+
+        for i in range(self.n_layers):
+            self.single_layer(self.params[i], last=(i==self.n_layers-1))
+
+        return qml.probs(wires=range(self.output_qubits)) # |00>, |01>, |10>, |11> 이런식으로 모든 basis들의 확률값을 반환
+
+    def forward(self, generator_input):
+        generator_output = [self.generator_circuit_qnode(single_in) for single_in in generator_input]  # (BATCH_SIZE, 2**output_qubits)
+        generator_output = torch.stack(generator_output)  # (BATCH_SIZE, 2**output_qubits)
+        return generator_output
+    
+    def parameters(self):
+        return [self.params]
